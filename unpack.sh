48fd3fa79d9dc8684603b1c7f98a48f55aefca36
ufrs12 <ufrs12@yandex.ru>
2024-04-12 16:41:07 +0300

    1

--- unpack.sh
+++
@@ -1,5 +0,0 @@
-# Копируем сетевые настройки из репозитория в overlay
-cp ./overlay/network/interfaces ../build/userpatches/overlay/network/interfaces
-
-# Копируем список добавляемых пакетов из репозитория userpatches
-cp ./lib.config ../build/userpatches/lib.config
