1c5bc8c5a51a33133e44288b062754d70f6c3970
ufrs12 <ufrs12@yandex.ru>
2024-04-12 03:39:46 +0300

    Update customize-image.sh

--- customize-image.sh
+++ customize-image.sh
@@ -1,3 +1,3 @@
-cp /tmp/overlay/network/interfaces /etc/network
-rm /etc/profile.d/armbian-check-first-login.sh
-rm /etc/profile.d/armbian-check-first-login-reboot.sh
+`cp /tmp/overlay/network/interfaces /etc/network` - копируем настройки  
+`rm /etc/profile.d/armbian-check-first-login.sh` - удаляем файл первичных авторизаций (остается root/1234)  
+`rm /etc/profile.d/armbian-check-first-login-reboot.sh` - удаляем файл первичных авторизаций (остается root/1234)
